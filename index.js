605798ca0acb29faba710f010bca504fe4d33787
fedabeqain <feda.beqain@gmail.com>
2023-10-12 00:37:49 +0300

    labcompleted

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const tutorials = [
+let tutorials = [
   'what does the this keyword mean?',
   'What is the Constructor OO pattern?',
   'implementing Blockchain Web API',
@@ -11,6 +11,21 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
+const titleCased = tutorials.map(element => { 
+  let str = element.split(",");
+  const NewFunction=str.map(element => {
+    let string = element.split(" ");
+    let arr ;
+    let array= []
+  for (let i = 0 ; i < string.length ; i++)
+   {
+    string[i]= string[i][0].toUpperCase()+string[i].substring(1);
+    }
+    arr = (string.join(" "));
+ console.log(arr)
+return str;
+  } )
   return tutorials
-}
+})
+
+titleCased;
